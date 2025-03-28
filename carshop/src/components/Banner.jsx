77873de52a66b286b62065e50c5cca9e1a5015dd
zhbforum <lyosha.minakoff@gmail.com>
2025-03-28 02:53:58 +0100

    Test animations for banner and header added

--- carshop/src/components/Banner.jsx
+++ carshop/src/components/Banner.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 export default function Banner() 
@@ -12,26 +13,32 @@ export default function Banner()
     fetch("/api/brands")
       .then((res) => res.json())
       .then((data) => setBrands(data))
-      .catch((error) =>
-        console.error("Error while loading brands:", error)
-      );
+      .catch((error) => console.error("Error while loading brands:", error));
   }, []);
 
   return (
     <div className="mt-14 flex justify-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-30 max-w-7xl">
-        {brands.map((brand) => (
+        {brands.map((brand, index) => (
           <Link key={brand.id} href={`/products/${brand.slug}`}>
-            <div className="relative w-80 h-80 rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition duration-300 cursor-pointer">
+            <motion.div
+              className="relative w-80 h-80 rounded-2xl overflow-hidden shadow-md hover:shadow-xl transition duration-300 cursor-pointer flex items-center justify-center bg-white"
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 5 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+              viewport={{ once: true, amount: 0.2 }}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.98 }}
+            >
               <img
                 src={brand.image}
                 alt={brand.slug}
-                className="w-full h-full object-contain p-4"
+                className="max-h-[75%] max-w-[75%] object-contain"
               />
-              <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-black whitespace-nowrap text-sm font-semibold">
-                {brand.name}
+              <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-black whitespace-nowrap text-sm font-semibold text-center">
+                Підкрилки для автомобілей марки <br /> {brand.name}
               </div>
-            </div>
+            </motion.div>
           </Link>
         ))}
       </div>
--- carshop/src/components/Header.jsx
+++ carshop/src/components/Header.jsx
@@ -1,31 +1,47 @@
 "use client";
 
 import Link from "next/link";
-import { ShoppingCart } from 'lucide-react';
+import { ShoppingCart } from "lucide-react";
+import { motion } from "framer-motion";
 
-export default function Header() {
+export default function Header() 
+{
   return (
-    <header className="bg-lime-600 shadow-md py-4 px-6 flex justify-between items-center sticky top-0 z-50">
-      <Link href="/"><h1 className="text-2xl font-bold text-gray-900">
-        CarShop
-      </h1> </Link>
+    <motion.header
+      initial={{ y: -50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.6 }}
+      className="bg-lime-600 shadow-md py-4 px-6 flex justify-between items-center sticky top-0 z-50"
+    >
+      <Link href="/">
+        <h1 className="text-2xl font-bold text-gray-900">CarShop</h1>
+      </Link>
       <nav className="flex items-center space-x-4">
-        <Link href="/products" className="text-gray-900 hover:text-green-100 transition-colors duration-300">
+        <Link
+          href="/products"
+          className="text-gray-900 hover:text-green-100 transition-colors duration-300"
+        >
           Каталог
         </Link>
-        <Link href="/contacts" className="text-gray-900 hover:text-green-100 transition-colors duration-300">
+        <Link
+          href="/contacts"
+          className="text-gray-900 hover:text-green-100 transition-colors duration-300"
+        >
           Контакт
         </Link>
-        <Link href="/reviews" className="text-gray-900 hover:text-green-100 transition-colors duration-300">
+        <Link
+          href="/reviews"
+          className="text-gray-900 hover:text-green-100 transition-colors duration-300"
+        >
           Відгуки
         </Link>
         <Link href="/cart">
-  <div className="flex items-center space-x-2  text-gray-900 hover:text-green-100 transition-colors duration-300 cursor-pointer">
-    <span>Кошик</span>
-    <ShoppingCart className="w-5 h-5" />
-  </div>
-</Link>
+          <div className="flex items-center space-x-2 text-gray-900 hover:text-green-100 transition-colors duration-300 cursor-pointer">
+            <span>Кошик</span>
+            <ShoppingCart className="w-5 h-5" />
+          </div>
+        </Link>
       </nav>
-    </header>
+    </motion.header>
   );
 }
