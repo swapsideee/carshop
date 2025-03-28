26285eb746d9f4404d191c939c71d1562af5c0bd
zhbforum <lyosha.minakoff@gmail.com>
2025-03-28 02:54:59 +0100

    Small prettier changes in footer

--- carshop/src/components/Footer.jsx
+++ carshop/src/components/Footer.jsx
@@ -1,11 +1,24 @@
 export default function Footer() 
 {
-    return (
-        <footer className="bg-lime-600 shadow-md text-gray-900 mt-16 py-6 text-center text-sm">
-            <p>@ {new Date().getFullYear()} CarShop. Усі права захищені</p>
-            <p className="mt-1">
-                Зв'язок: <a href="mailto:Vadi-Avto@ukr.net" className="text-gray-900 hover:text-gray-700">Vadi-Avto@ukr.net</a> | Тел: <a href="tel:+380961365299" className="text-gray-900 hover:text-gray-700">+38 096 136 5299</a>
-            </p>
-        </footer>
-    );
+  return (
+    <footer className="bg-lime-600 shadow-md text-gray-900 mt-16 py-6 text-center text-sm">
+      <p>@ {new Date().getFullYear()} CarShop. Усі права захищені</p>
+      <p className="mt-1">
+        Зв'язок:{" "}
+        <a
+          href="mailto:Vadi-Avto@ukr.net"
+          className="text-gray-900 hover:text-gray-700"
+        >
+          Vadi-Avto@ukr.net
+        </a>{" "}
+        | Тел:{" "}
+        <a
+          href="tel:+380961365299"
+          className="text-gray-900 hover:text-gray-700"
+        >
+          +38 096 136 5299
+        </a>
+      </p>
+    </footer>
+  );
 }
