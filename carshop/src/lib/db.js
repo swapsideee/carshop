dd1de473f52b09373e25889888c1191a5da08ba4
zhbforum <lyosha.minakoff@gmail.com>
2025-07-11 01:20:50 +0200

    changes in db

--- carshop/src/lib/db.js
+++ carshop/src/lib/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 export const getDB = async () => {
-  return await mysql.createPool({
+  return mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
