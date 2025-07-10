d77cfcee832fd1d8e7e74a2a55b5f99ae69504a3
zhbforum <lyosha.minakoff@gmail.com>
2025-07-10 02:27:28 +0200

    Testing for railway

--- carshop/src/app/api/brands/route.js
+++ carshop/src/app/api/brands/route.js
@@ -1,8 +1,9 @@
-import { db } from '@/lib/db';
+import { getDB } from '@/lib/db';
 import { getAllBrands } from '@/lib/queries/brands';
 import { ErrorHandler } from '@/lib/utils/errorHandler';
 
 export const GET = ErrorHandler(async () => {
+    const db = getDB();
     const [rows] = await db.query(getAllBrands);
     return Response.json(rows);
 });
--- carshop/src/lib/db.js
+++ carshop/src/lib/db.js
@@ -1,9 +1,11 @@
 import mysql from 'mysql2/promise';
 
-export const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: 3306
-});
+export const getDB = () => {
+    return mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        port: 3306
+    });
+};
