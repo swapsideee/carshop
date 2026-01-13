1b1bb415daa5a7ed738140c420bfddcf8190abb1
zhbforum <lyosha.minakoff@gmail.com>
2026-01-13 03:41:37 +0100

    chore(content): update branding and contacts

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { Building, Clock, Mail, MapPin, Phone } from 'lucide-react';
 
 const CompanyCard = () => {
@@ -14,20 +15,20 @@ const CompanyCard = () => {
               href="mailto:Vadi-Avto@ukr.net"
               className="text-gray-700 underline hover:text-gray-500"
             >
-              vadiavtoua@gmail.com
+              example@gmail.com
             </a>
           </p>
 
           <p className="text-lg text-gray-700 flex items-center gap-2">
             <Phone className="w-5 h-5 shrink-0 self-start mt-1" />
-            <a href="tel:+380961365299" className="text-gray-700 underline hover:text-gray-500">
-              +38 096 136 5299
+            <a href="tel:+381234567899" className="text-gray-700 underline hover:text-gray-500">
+              +38 123 456 7899
             </a>
           </p>
 
           <p className="text-lg text-gray-700 flex items-center gap-2">
             <MapPin className="w-5 h-5 shrink-0 self-start mt-1" />
-            Авторинок &quot;Лоск&quot;, 13 ряд, 9 місце
+            вул. Приблизна, 10
           </p>
 
           <p className="text-lg text-gray-700 flex items-center gap-2">
@@ -44,7 +45,7 @@ const CompanyCard = () => {
         <div className="w-full h-72 sm:h-80 rounded-lg overflow-hidden">
           <iframe
             title="Google Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2565.2493535821866!2d36.124826115715536!3d49.9701940794211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41279f5fa4a55557%3A0x444a88900a51d144!2z0JDQstGC0L7QutCw0YDQvtCyINCf0YDQtdC90YLRgNC-0Lkg0JvQvtCz0LTQsNGC0LXRgdC60LjQuSDQstC10YDRjNC60LAg0LrQvtCz0LTQsA!5e0!3m2!1suk!2sde!4v1718474344135!5m2!1suk!2sde"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d41044.94908353241!2d36.25218043736826!3d49.98681002010195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4127a09f63ab0f8b%3A0x2d4c18681aa4be0a!2z0KXQsNGA0YzQutC-0LIsINCl0LDRgNGM0LrQvtCy0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsINCj0LrRgNCw0LjQvdCw!5e0!3m2!1sru!2sde!4v1768246895936!5m2!1sru!2sde"
             width="100%"
             height="100%"
             style={{ border: 0 }}
