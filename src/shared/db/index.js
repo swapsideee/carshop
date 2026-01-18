75a9b61906b15b249d9869da1710bfb0eed075f2
zhbforum <lyosha.minakoff@gmail.com>
2026-01-18 05:18:46 +0100

    refactor(shared): introduce shared db module

---
+++ src/shared/db/index.js
@@ -0,0 +1,1 @@
+export { getDB } from './mysql';
---
+++ src/shared/db/mysql.js
@@ -0,0 +1,21 @@
+import 'server-only';
+
+import mysql from 'mysql2/promise';
+
+let pool;
+
+export const getDB = async () => {
+  if (!pool) {
+    pool = mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT) || 3306,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+  }
+  return pool;
+};
