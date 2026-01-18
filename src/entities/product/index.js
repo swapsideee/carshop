2f78efcd7cd75a63ee654a135afa370569ad8ab0
zhbforum <lyosha.minakoff@gmail.com>
2026-01-18 21:41:41 +0100

    refactor(product): move useProduct hook to entities public api

--- src/entities/product/index.js
+++ src/entities/product/index.js
@@ -1,2 +1,4 @@
+export * from './api/client';
 export * as productSelectors from './model/selectors';
+export * from './model/useProduct';
 export { default as ProductCard } from './ui/ProductCard/ProductCard';
