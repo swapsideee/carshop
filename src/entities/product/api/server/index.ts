a9576189d53b47125891959e14675d7bb6f81c80
zhbforum <lyosha.minakoff@gmail.com>
2026-02-07 01:26:30 +0100

    chore: apply lint fixes

--- src/entities/product/api/server/index.ts
+++ src/entities/product/api/server/index.ts
@@ -1,4 +1,4 @@
 import 'server-only';
 
 export type { GetProductsPagedArgs } from './repo';
-export { getProductDetailsById, getProductsForSelect,getProductsPaged } from './repo';
+export { getProductDetailsById, getProductsForSelect, getProductsPaged } from './repo';
